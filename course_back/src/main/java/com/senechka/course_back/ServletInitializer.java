37309f00d481c84d65d48acac02e7b50f602609d
Senechka <arseny.konovalov@tune-it.ru>
2025-01-23 19:00:06 +0300

    DEV-0 Started Security config

--- course_back/src/main/java/com/senechka/course_back/ServletInitializer.java
+++ course_back/src/main/java/com/senechka/course_back/ServletInitializer.java
@@ -2,6 +2,8 @@ package com.senechka.course_back;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
